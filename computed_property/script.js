f514cb0ef8d84a00afe87c46961ef3912142f89b
Tuhame Astone <astonetuhame@gmail.com>
2021-12-12 21:31:19 +0300

    computed property

---
+++ computed_property/script.js
@@ -0,0 +1,21 @@
+Vue.createApp({
+  data() {
+    return {
+      author: {
+        name: 'John Doe',
+        books: [
+          'Vue 2 - Advanced Guide',
+          'Vue 3 - Basic Guide',
+          'Vue 4 - The Mystery'
+        ]
+      }
+    }
+  },
+  computed: {
+    // a computed getter
+    publishedBooksMessage() {
+      // `this` points to the vm instance
+      return this.author.books.length > 0 ? 'Yes' : 'No'
+    }
+  }
+}).mount('#computed-basics')
